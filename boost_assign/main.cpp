753ff1a659570b2707f083879d4b630d244895f6
Joohwi Lee <jlee@autox.ai>
2019-08-02 01:12:14 -0700

    New code

---
+++ boost_assign/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  boost_assign
+//
+//  Created by Joohwi Lee on 8/2/19.
+//  Copyright © 2019 Joohwi Lee. All rights reserved.
+//
+
+#include <iostream>
+#include "boost/assign"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
