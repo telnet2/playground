3c43f0e2efce5d696128c22703f8c4a3009760d4
Joohwi Lee <jlee@autox.ai>
2019-12-29 11:57:07 -0800

    Line counter example

---
+++ spark/build.sbt
@@ -0,0 +1,4 @@
+name := "Line Counter"
+version := "1.0"
+scalaVersion := "2.11.8"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.4.4"
---
+++ spark/src/main/scala/demo/LineCounter.scala
@@ -0,0 +1,16 @@
+package mapping
+
+/* LineCounter.scala */
+import org.apache.spark.sql.SparkSession
+
+object LineCounter {
+  def main(args: Array[String]) {
+    val logFile = "README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Line Counter").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
+}
