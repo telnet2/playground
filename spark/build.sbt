5cb4840503dbd68b9416388d1d2a202b020e62dd
Joohwi Lee <jlee@autox.ai>
2020-01-01 13:23:41 -0800

    s3 retrieval and proto decoding

--- spark/build.sbt
+++ spark/build.sbt
@@ -1,4 +1,31 @@
+// import scalapb.compiler.Version.scalapbVersion
+
 name := "Line Counter"
 version := "1.0"
-scalaVersion := "2.11.8"
+scalaVersion := "2.11.12"
+// showSuccess := false
+logLevel := Level.Error
+
+// Compile Protobufs
+PB.targets in Compile := Seq(
+  scalapb.gen() -> (sourceManaged in Compile).value
+)
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.4.4"
 libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.4.4"
+libraryDependencies += "org.apache.hadoop" % "hadoop-aws" % "2.7.3"
+libraryDependencies += "com.amazonaws" % "aws-java-sdk" % "1.7.4"
+
+libraryDependencies += "com.google.protobuf" % "protobuf-java" % "3.8.0"
+// libraryDependencies += "com.thesamet.scalapb" %% "scalapb-runtime" % scalapbVersion % "protobuf"
+libraryDependencies += "com.thesamet.scalapb" %% "scalapb-runtime" % "0.9.3"
+
+// libraryDependencies += "com.thesamet.scalapb" %% "scalapb-json4s" % "0.9.3"
+// libraryDependencies += "com.amazonaws" % "aws-java-sdk-core" % "1.11.699"
+// libraryDependencies += "com.amazonaws" % "aws-java-sdk-s3" % "1.11.699"
+
+// Compile / unmanagedJars ++= {
+//   val base = file("/repo/spark/jars")
+//   val customJars = (base ** "*.jar")
+//   customJars.classpath
+// }
--- spark/src/main/scala/demo/LineCounter.scala
+++ spark/src/main/scala/demo/LineCounter.scala
@@ -1,16 +1,40 @@
 package mapping
 
 /* LineCounter.scala */
+// import org.apache.spark._
+import java.util.Base64
 import org.apache.spark.sql.SparkSession
+import msg.mapreduce.LidarFrame
+import scalapb.TextFormat
+// import org.apache.spark.sql.SparkSession
 
 object LineCounter {
   def main(args: Array[String]) {
-    val logFile = "README.md" // Should be some file on your system
-    val spark = SparkSession.builder.appName("Line Counter").getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    val decoder = Base64.getDecoder()
+    val spark = SparkSession.builder
+      .appName("Line Counter")
+      .config("spark.master", "local")
+      .getOrCreate()
+
+    val baseUrl =
+      "s3a://autox-mapping/metadata/2019_12_31_13_52_30_map_update.meta"
+
+    val logData = spark.read
+      .option("header", "false")
+      .option("delimiter", " ")
+      .csv(s"$baseUrl/part-00000")
+      .cache()
+
+    val firstRow = logData.head()
+
+    val proto = LidarFrame.parseFrom(decoder.decode(firstRow.getAs[String](1)))
+    println(TextFormat.printToString(proto))
+
+    // logData.filter(row => row.getAs[String](1).contains("a")).count()
+
+    // println(s"Lines with a: $numAs")
+    // val numBs = logData.filter(line => line.contains("b")).count()
+    // println(s"Lines with a: $numAs, Lines with b: $numBs")
     spark.stop()
   }
 }
